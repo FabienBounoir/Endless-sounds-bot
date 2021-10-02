78987d94874f99f9d905e34b9990261cf524f755
fabienbounoir <fabien.bounoir@epitech.eu>
2021-10-03 01:52:07 +0200

    correction bug

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const Discord = require('discord.js');
 const { MessageEmbed, MessageAttachment } = require('discord.js');
 const client = new Discord.Client(); 
 const config = require('./config.json');
-const axios = require('axios').default;
-const ytdl = require('ytdl-core');
 const fs = require("fs")
 const mongoose = require('mongoose');
 var ComptePlayEgirl = require('./model/ComptePlayEgirl.js');
@@ -62,8 +60,8 @@ function play()
 
     channel.join().then(connection => {
         musiqueEnCours = musiqueList[parseInt(Math.random() * musiqueList.length)]
-        console.log(musiqueEnCours)
-        dispatcher = connection.play( "./" + musiqueEnCours,{volume: 0.5,});
+
+        dispatcher = connection.play( "./musique/" + musiqueEnCours,{volume: 0.5,});
         
         dispatcher.on('finish', () => {
             ajoutPlay()
@@ -128,17 +126,17 @@ function format(time) {
 client.on('message', async message => {
 
     //commande qui renvoie des stats sur le bot (nbplay / nbTime)
-    if(message.content.toLowerCase() == "!stategirl")
+    if(message.content.toLowerCase() == "!statpike")
     {
         ComptePlayEgirl.find().exec((erreur, playCount) => {
             if (erreur) return console.error(err);
             
 
-            if(musiqueEnCours == "pike-e-girl.mp3")
+            if(musiqueEnCours == "pike-e-girl.mp3") //62724 play de egirl de pike avant ajoue autre musique
             {
                 const XpEmbed = new MessageEmbed()
-                .setColor('#f372ff')
-                .setTitle("Nombres de Musique Joué par Egirl'Bot")
+                .setColor('#7100fc')
+                .setTitle("Nombres de Musique joué par Pike'Bot")
                 .setURL('https://www.youtube.com/watch?v=nvYi3XlP8sk')
                 .setDescription("**Nb Play: " + playCount[0].nombrePlay + "**")
                 .addField('Équivalant à **' + format(playCount[0].nombrePlay * 209) + "**", '\u200B', true)
@@ -150,8 +148,8 @@ client.on('message', async message => {
             else
             {
                 const XpEmbed = new MessageEmbed()
-                .setColor('#f372ff')
-                .setTitle("Nombres de Musique Joué par Egirl'Bot")
+                .setColor('#7100fc')
+                .setTitle("Nombres de Musique joué par Pike'Bot")
                 .setURL('https://www.youtube.com/watch?v=ltuAhZH6PmA')
                 .setDescription("**Nb Play: " + playCount[0].nombrePlay + "**")
                 .addField('Équivalant à **' + format(playCount[0].nombrePlay * 209) + "**", '\u200B', true)
@@ -164,7 +162,7 @@ client.on('message', async message => {
     }
 
     //commande qui reload le bot
-    if(message.content.toLowerCase() == "!reloadegirl")
+    if(message.content.toLowerCase() == "!reloadpike")
     {
         if(message.author.id != administrateur)
         {
@@ -176,7 +174,7 @@ client.on('message', async message => {
 
             setTimeout(() => {
                 play();
-            }, 2000);
+            }, 1000);
 
             message.member.voice.channel.leave();
             message.channel.send("Reload En Cours...")
@@ -188,7 +186,10 @@ client.on('message', async message => {
     if(message.content.toLowerCase() == "!paroleegirl")
     {
         try {
-            message.channel.send(`REFRAIN:
+const embed = new Discord.MessageEmbed()
+.setFooter("Pike'Bot",client.user.avatarURL())
+.setTitle("Parole Pike Egirl")
+.setDescription(`REFRAIN:
 
 Depuis tout petit je sais que j'aime les E-Girls
 J'pourrais les regarder pendant des heures
@@ -220,9 +221,7 @@ PONT:
 Ooooh Hellsey épouse-moi,
 Yayaya, 
 Ohohohohoh ouais,
-Hellsey marie-moi`);
-message.channel.send(`
-Parole E girl:
+Hellsey marie-moi
 
 REFRAIN:
 
@@ -268,6 +267,88 @@ Yayayaya, Hellsey épouse-moi
 
 Yayaya, Hellsey marie-moi
 Yayayaya, Hellsey épouse-moi`)
+message.channel.send(embed)
+
+//             message.channel.send(`REFRAIN:
+
+// Depuis tout petit je sais que j'aime les E-Girls
+// J'pourrais les regarder pendant des heures
+// Jusqu'à c'que j'meurs
+// Je lui donnerais tout je suis un admirateur (oh oui mon coeur)
+// Je vais lui faire passer l'aspirateur
+// Pour moi elle fera tout le dur labeur
+// t'inquiète pas qu'avec moi elle en passera des heures
+// Yayaya, Hellsey marie-moi
+// Yayayaya, Hellsey épouse-moi
+
+// 1ER COUPLET:
+
+// Elle a les cheveux bleus et roses
+// Un peu comme une gomme
+// Avec elle la vie n'est plus morose 
+// Depuis qu'elle efface mes fautes
+// Elle ressemble à Belle Delphine mais moi ça me dérange pas
+// Tu sais j'aime ce genre de filles qui vend son eau de bain
+// Kenne-Kennedy, tu manques mon amie
+// On se verra au paradis
+// Toi moi et Billy
+// Kenne-Kennedy, tu manques mon amie
+// Depuis que t'es bannie
+// Zebi zebi zebi zebi
+
+// PONT:
+
+// Ooooh Hellsey épouse-moi,
+// Yayaya, 
+// Ohohohohoh ouais,
+// Hellsey marie-moi`);
+// message.channel.send(`
+// Parole E girl:
+
+// REFRAIN:
+
+// Depuis tout petit je sais que j'aime les E-Girls
+// J'pourrais les regarder pendant des heures
+// Jusqu'à c'que j'meurs
+// Je lui donnerais tout je suis un admirateur (oh oui mon coeur)
+// Je vais lui faire passer l'aspirateur
+// Pour moi elle fera tout le dur labeur
+// t'inquiète pas qu'avec moi elle en passera des heures
+// Yayaya, Hellsey marie-moi
+// Yayayaya, Hellsey épouse-moi
+
+// 2E COUPLET:
+
+// J'suis trop accro à toi comme du cannabis
+// J'aimerais dire des choses mais j'veux pas finir banni
+// Donc j'vais dire que t'es belle
+// Et que j'ai envie de m'étouffer entre tes.. (fesses)
+// Et vu que j'ai la niaque, j'vais aussi t'exploser ton gros fiak
+// T'es trop fraîche j'ai envie de te manger comme un steak
+// Tu es ma planète
+// Sans toi je n'suis plus un mec
+
+// PONT:
+
+// Ooooh Hellsey épouse-moi,
+// Yayaya, 
+// Ohohohohoh ouais,
+// Hellsey marie-moi
+
+// REFRAIN:
+
+// Depuis tout petit je sais que j'aime les E-Girls
+// J'pourrais les regarder pendant des heures
+// Jusqu'à c'que j'meurs
+// Je lui donnerais tout je suis un admirateur (oh oui mon coeur)
+// Je vais lui faire passer l'aspirateur
+// Pour moi elle fera tout le dur labeur
+// t'inquiète pas qu'avec moi elle en passera des heures
+// Yayaya, Hellsey marie-moi
+// Yayayaya, Hellsey épouse-moi
+
+// Yayaya, Hellsey marie-moi
+// Yayayaya, Hellsey épouse-moi`)
         } catch (error) {
             console.log(error)
         }
